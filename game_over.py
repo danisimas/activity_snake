e6b53017db83ffe69c3b1042a4daec756830a96d
Amós do Carmo Moreira Filho <adcmf.snf20@uea.edu.br>
2021-05-30 20:42:05 -0400

    Adjust volume

--- game_over.py
+++ game_over.py
@@ -13,6 +13,7 @@ click = False
 
 def game_over(score):
     global click
+    flag = 0
     while not click:
         window.fill((144, 238, 144))
         font = pygame.font.Font("assets/Vermin Vibes 1989.ttf", 48)
@@ -29,13 +30,19 @@ def game_over(score):
         text_1_rect.center = (300, 250)
         text_2_rect.center = (300, 350)
         text_3_rect.center = (300, 450)
+        if flag == 0:
+            death_sound = pygame.mixer.Sound('assets/death sound.wav')
+            death_sound.set_volume(0.1)
+            death_sound.play()
+            flag = 1
+
 
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    menu.menu()
+                    menu()
                     click = True
-        click = False
+
         window.blit(text_1, text_1_rect)
         window.blit(text_2, text_2_rect)
         window.blit(text_3, text_3_rect)
@@ -43,9 +50,10 @@ def game_over(score):
 
 
 def set_click():
-    global click
     click = False
 
 
 if __name__ == '__main__':
     game_over(score)
+
+
